1050e047e8fe879e009509701fd3ee846333fecb
José Núñez <79015124+josiext@users.noreply.github.com>
2023-10-17 15:22:50 -0300

    updates

---
+++ microfront-navbar/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="container">
+      <p>TBX Navbar | Micro-frontend</p>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ minifront-invoices/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Button from "remoteApp/Button";
+
+function App() {
+  return (
+    <>
+      Este boton viene del microfrontend:
+      <Button />
+    </>
+  );
+}
+
+export default App;
